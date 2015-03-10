da676d293c0c98c6cc153682786c4aef76d7dc80
David Baker <dbaker3@gmail.com>
2015-03-10 15:05:04 -0400

    fixed hostname

--- canvas-search-template.php
+++ canvas-search-template.php
@@ -5,10 +5,10 @@ Template Name: Canvas Searchbox
 ?>
 <html>
    <head>
-      <script type='text/javascript' src='http://localhost/wp-includes/js/jquery/jquery.js'></script>
+      <script type='text/javascript' src='http://library.milligan.edu/wp-includes/js/jquery/jquery.js'></script>
       <link rel='stylesheet' id='open-sans-css'  href='//fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&#038;subset=latin%2Clatin-ext&#038;ver=4.1.1' type='text/css' media='all' />
-      <script type='text/javascript' src='http://localhost/wp-content/plugins/phw-searchbox/searchbox.js'></script>
-      <link rel='stylesheet' id='searchboxcss-css'  href='http://localhost/wp-content/plugins/phw-searchbox/searchbox.css' type='text/css' media='all' />
+      <script type='text/javascript' src='http://library.milligan.edu/wp-content/plugins/phw-searchbox/searchbox.js'></script>
+      <link rel='stylesheet' id='searchboxcss-css'  href='http://library.milligan.edu/wp-content/plugins/phw-searchbox/searchbox.css' type='text/css' media='all' />
       <script type='text/javascript' src='http://support.ebscohost.com/eit/scripts/ebscohostsearch.js'></script>
       <style>
           html {font-family:"Open Sans";}
