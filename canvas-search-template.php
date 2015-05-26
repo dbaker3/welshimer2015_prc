adcc557af78e158568382568914ea6bae144d41f
dbaker3 <dbaker3@gmail.com>
2015-05-26 20:21:08 +0000

    Added chat widget to canvas template

--- canvas-search-template.php
+++ canvas-search-template.php
@@ -12,7 +12,7 @@ wp_dequeue_script("iframenonewtab");
    html {margin-top: 0 !important;}
    body {background-image: none;}
    #wpadminbar, #masthead, #primary-menu, .entry-title, #mobile-menu-wrapper, footer {display:none !important;}
-   .site-content {min-height: 0 !important};
+   .site-content {min-height: 0 !important;}
 </style>
 
       <div id="primary" class="site-content">
@@ -28,6 +28,14 @@ wp_dequeue_script("iframenonewtab");
 					<div class="entry-content">
 
 						<?php the_content(); ?>
+						
+						<div style="float: right; position: relative; top: -12em; right: -12em;">
+							<h1 class="chat-icon">Need Help?</h1>
+							<div class="needs-js">
+								Chat service is unavailable. Please ensure that <a href="http://enable-javascript.com/">Javascript is enabled</a>
+							</div>
+						</div>
+						
 						<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'welshimer2013' ), 'after' => '</div>' ) ); ?>
 						<?php edit_post_link( __( 'Edit', 'welshimer2013' ), '<span class="edit-link">', '</span>' ); ?>
 					</div><!-- .entry-content -->
