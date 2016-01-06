e92a6080f6e05bbddacdc60249d7b6163270d3f1
dbaker3 <dbaker3@gmail.com>
2016-01-06 21:21:55 +0000

    changed email address back

--- form-interlibrary-loan-template.php
+++ form-interlibrary-loan-template.php
@@ -132,7 +132,7 @@ remove_filter('the_content', 'wpautop'); // Keeps WP from adding the annoying <p
 		if(!isset($hasError)) {
         // email request to ILL account
         	if ($patron_program == "Graduate - Seminary") {
-	    		$emailTo = 'mc_ill@milligan.edu';  //'SEArndt@milligan.edu,JMWade@milligan.edu';
+	    		$emailTo = 'SEArndt@milligan.edu,JMWade@milligan.edu';
         	}
 	    	else {
 	    		$emailTo = 'mc_ill@milligan.edu';
